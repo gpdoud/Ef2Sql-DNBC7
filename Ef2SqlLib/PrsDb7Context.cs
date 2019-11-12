fd75f3ae5f9d6c2c565c4cc460689133502265c5
Greg Doud <gpdoud@gmail.com>
2019-11-12 16:35:04 -0500

    "Final commit"

--- Ef2SqlLib/PrsDb7Context.cs
+++ Ef2SqlLib/PrsDb7Context.cs
@@ -2,17 +2,13 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace Ef2SqlLib
-{
-    public partial class PrsDb7Context : DbContext
-    {
-        public PrsDb7Context()
-        {
+namespace Ef2SqlLib {
+    public partial class PrsDb7Context : DbContext {
+        public PrsDb7Context() {
         }
 
         public PrsDb7Context(DbContextOptions<PrsDb7Context> options)
-            : base(options)
-        {
+            : base(options) {
         }
 
         public virtual DbSet<Products> Products { get; set; }
@@ -21,21 +17,17 @@ namespace Ef2SqlLib
         public virtual DbSet<Users> Users { get; set; }
         public virtual DbSet<Vendors> Vendors { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            if(!optionsBuilder.IsConfigured) {
                 optionsBuilder.UseLazyLoadingProxies();
                 optionsBuilder.UseSqlServer("server=localhost\\sqlexpress;database=PrsDb7;trusted_connection=true;");
             }
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
 
-            modelBuilder.Entity<Products>(entity =>
-            {
+            modelBuilder.Entity<Products>(entity => {
                 entity.HasIndex(e => e.PartNbr)
                     .HasName("UQ__Products__DAFC0C1E4271F077")
                     .IsUnique();
@@ -51,8 +43,7 @@ namespace Ef2SqlLib
                     .HasConstraintName("FK__Products__Vendor__5441852A");
             });
 
-            modelBuilder.Entity<RequestLines>(entity =>
-            {
+            modelBuilder.Entity<RequestLines>(entity => {
                 entity.Property(e => e.Quantity).HasDefaultValueSql("((1))");
 
                 entity.HasOne(d => d.Product)
@@ -68,8 +59,7 @@ namespace Ef2SqlLib
                     .HasConstraintName("FK__RequestLi__Reque__5CD6CB2B");
             });
 
-            modelBuilder.Entity<Requests>(entity =>
-            {
+            modelBuilder.Entity<Requests>(entity => {
                 entity.Property(e => e.DeliveryMode).HasDefaultValueSql("('PickUp')");
 
                 entity.Property(e => e.Status).HasDefaultValueSql("('NEW')");
@@ -81,15 +71,13 @@ namespace Ef2SqlLib
                     .HasConstraintName("FK__Requests__UserId__59FA5E80");
             });
 
-            modelBuilder.Entity<Users>(entity =>
-            {
+            modelBuilder.Entity<Users>(entity => {
                 entity.HasIndex(e => e.Username)
                     .HasName("UQ__Users__536C85E4539824FB")
                     .IsUnique();
             });
 
-            modelBuilder.Entity<Vendors>(entity =>
-            {
+            modelBuilder.Entity<Vendors>(entity => {
                 entity.HasIndex(e => e.Code)
                     .HasName("UQ__Vendors__A25C5AA726F9572A")
                     .IsUnique();
